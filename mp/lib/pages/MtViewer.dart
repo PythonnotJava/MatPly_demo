cc32e9429b874c2c6548697cc23ad7d56f1d485d
PythonnotJava <2565497078@qq.com>
2024-09-24 10:03:27 +0800

    demo

--- mp/lib/pages/MtViewer.dart
+++ mp/lib/pages/MtViewer.dart
@@ -1,16 +1,13 @@
-import 'package:flutter/cupertino.dart' show Column, ConnectionState, Container, Image, MainAxisAlignment, SizedBox;
-import 'package:flutter/material.dart' show AsyncSnapshot, BuildContext, Center, CircularProgressIndicator, Colors, Expanded, FutureBuilder, StatelessWidget, Text, Widget;
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_svg/flutter_svg.dart';
+
+import 'dart:io';
 class Mtviewer extends StatelessWidget {
   const Mtviewer({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      'assets/mt.svg',
-      placeholderBuilder: (BuildContext context) => Container(
-          child: const CircularProgressIndicator()),
+    return Center(
+
     );
   }
 }
